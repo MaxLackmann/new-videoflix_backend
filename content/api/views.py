5162d9ca1aea283634ed594531dceaac8e1afa1e
MaxLackmann <lackmann_max@hotmail.com>
2025-07-09 16:43:55 +0200

    test

--- content/api/views.py
+++ content/api/views.py
@@ -6,14 +6,12 @@ from content.api.serializers import VideoSerializer
 from rest_framework_simplejwt.authentication import JWTAuthentication
 
 class VideoListView(APIView):
-    permission_classes = [JWTAuthentication]
     def get(self, request):
         videos = Video.objects.all()
         serializer = VideoSerializer(videos, many=True)
         return Response(serializer.data, status=status.HTTP_200_OK)
     
 class VideoDetailView(APIView):
-    permission_classes = [JWTAuthentication]
     def get(self, request, pk):
         video = Video.objects.get(pk=pk)
         serializer = VideoSerializer(video, context={'request': request})
